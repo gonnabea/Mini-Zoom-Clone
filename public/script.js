57ad0efed771e50a71568604df2224b44b58a76c
Choi Jiwon <winning1993@naver.com>
2020-09-14 23:58:47 +0900

    Fix Bug related to excution order

--- public/script.js
+++ public/script.js
@@ -1,6 +1,20 @@
 const socket = io("/")
+const videoGrid = document.getElementById("video-grid")
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3004",
+})
+const myVideo = document.createElement("video")
+myVideo.muted = true
 
-console.log(socket)
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+})
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id)
+})
 
 socket.emit("join-room", ROOM_ID, "Jiwon")
 
